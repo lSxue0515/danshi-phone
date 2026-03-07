d36f086fc99a82be6469e13aa740fe8be2ab80ac
lSxue0515 <3924872948@qq.com>
2026-03-07 08:37:03 +0800

    手机端入口

--- js/pwa.js
+++ js/pwa.js
@@ -1,15 +1,58 @@
 (function () {
     'use strict';
 
+    var installBtn = null;
+
+    function setInstallButtonVisible(visible) {
+        if (!installBtn) {
+            return;
+        }
+        installBtn.hidden = !visible;
+    }
+
+    function clearInstallPrompt() {
+        window.deferredPwaInstallPrompt = null;
+        setInstallButtonVisible(false);
+    }
+
+    function bindInstallButton() {
+        installBtn = document.getElementById('pwaInstallBtn');
+        if (!installBtn) {
+            return;
+        }
+
+        installBtn.addEventListener('click', function () {
+            var deferredPrompt = window.deferredPwaInstallPrompt;
+            if (!deferredPrompt) {
+                return;
+            }
+
+            deferredPrompt.prompt();
+            deferredPrompt.userChoice.then(function () {
+                clearInstallPrompt();
+            }).catch(function () {
+                clearInstallPrompt();
+            });
+        });
+    }
+
     if (!('serviceWorker' in navigator)) {
         console.log('[PWA] Service worker is not supported in this browser.');
         return;
     }
 
+    bindInstallButton();
+
     window.addEventListener('beforeinstallprompt', function (event) {
         event.preventDefault();
         window.deferredPwaInstallPrompt = event;
+        setInstallButtonVisible(true);
         console.log('[PWA] beforeinstallprompt captured.');
+    });
+
+    window.addEventListener('appinstalled', function () {
+        clearInstallPrompt();
+        console.log('[PWA] App installed.');
     });
 
     window.addEventListener('load', function () {
